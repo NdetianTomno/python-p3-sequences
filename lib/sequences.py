88d8f11aa2ed93c5cca3794d188c23a0cba27e60
Tomno <tomnoviv@gmail.com>
2023-05-18 16:46:13 +0300

    Completed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci = [0, 1]  
+
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    elif length == 2:
+        print(fibonacci)
+    else:
+        while len(fibonacci) < length:
+            next_number = fibonacci[-1] + fibonacci[-2]  
+            fibonacci.append(next_number)  
+        print(fibonacci)
+
